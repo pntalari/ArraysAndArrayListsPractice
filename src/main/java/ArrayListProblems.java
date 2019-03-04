39a855be064a7fd98e083d36955b9136d1cd7da6
Nirmala <nirmala.talari@gmail.com>
2019-03-03 21:06:20 -0500

    SOlutions

--- src/main/java/ArrayListProblems.java
+++ src/main/java/ArrayListProblems.java
@@ -1,35 +1,83 @@
+import java.util.Iterator;
 import java.util.List;
+import java.util.Collections;
+import java.util.ArrayList;
 
 public class ArrayListProblems {
 
     public Integer findMissingNumber(Integer min, Integer max, List<Integer> list) {
 
+        Integer[] arr = new Integer[max - min];
+        Integer num = 0;
 
-        return null;
+        for (int i = min; i <= max; i++) {
+            if (!list.contains(i)) {
+                num = i;
+            }
+        }
+        return num;
     }
+
+    //        ArrayList<String> arr = new ArrayList<>();
+//        Iterator<String> iter = arr.iterator();
+//        while (iter.hasNext()) {
+//            String ele = iter.next();
+//            if (true) {
+//                iter.remove();
 
 
     public List<Integer> findMultipleMissingNumbers(Integer min, Integer max, List<Integer> list) {
-
-        return null;
+        ArrayList<Integer> listR = new ArrayList<>();
+        for (int i = min; i <= max; i++) {
+            if (!list.contains(i)) {
+                listR.add(i);
+            }
+        }
+        return listR;
     }
 
 
     public List<String> findZipCoderNamesByFirstLetter(String firstLetter, List<String> zipcoders) {
+        ArrayList<String> retArr = new ArrayList<>();
 
-        return null;
+
+        for (String s:zipcoders
+             ) {
+            if (s.toLowerCase().startsWith(firstLetter.toLowerCase())) {
+                retArr.add(s);
+            }
+        }
+            return retArr;
     }
 
 
     public List<String> findZipCodersWhoseNamesContainLetter(String letter, List<String> zipcoders) {
 
-        return null;
+        ArrayList<String> retArr = new ArrayList<>();
+
+        for (String s:zipcoders
+        ) {
+            if (s.toLowerCase().contains(letter.toLowerCase())) {
+                retArr.add(s);
+            }
+        }
+        return retArr;
     }
 
 
     public List<String> removeDuplicateZipCoders(List<String> zipcoders) {
+        ArrayList<String> retArr = new ArrayList<>();
 
-        return null;
+
+        for (String s:zipcoders
+        ) {
+            if(!retArr.contains(s)){
+                retArr.add(s);
+            }
+        }
+        Collections.sort(retArr);
+        return retArr;
+
     }
 
 
--- src/main/java/ArrayProblems.java
+++ src/main/java/ArrayProblems.java
@@ -1,39 +1,105 @@
+import java.util.Arrays;
+
 public class ArrayProblems {
 
 
     public Integer findMissingNumber(Integer min, Integer max, Integer[] array) {
+        int totalSum = 0;
+        for(int i = min; i <= max; i++){
+            totalSum += i;
+        }
 
-        return null;
+        int arraySum = 0;
+        for(Integer i : array){
+            arraySum += i;
+        }
+
+        return totalSum - arraySum;
     }
-
 
 
     public Integer[] findMultipleMissingNumbers(Integer min, Integer max, Integer[] array) {
 
-        return null;
-    }
+        Arrays.sort(array);
+        int amountMissing = (max - min + 1) - array.length;
+        Integer[] missingNumbers = new Integer[amountMissing];
+        int index = 0;
 
+
+        for (int i = min; i <= max; i++) {
+            if (i - min - index < array.length) {
+                if (array[i - min - index] != i) {
+                    missingNumbers[index] = i;
+                    index++;
+                }
+            } else {
+                missingNumbers[index] = i;
+                index++;
+            }
+        }
+        return missingNumbers;
+    }
 
 
     public String[] findZipCoderNamesByFirstLetter(String firstLetter, String[] zipCoders) {
+        String[] names = new String[zipCoders.length];
+        int index = 0;
+        for (String s: zipCoders
+             ) {
+            if(s.toLowerCase().startsWith(firstLetter.toLowerCase())){
+                names[index] = s;
+                index++;
+            }
+        }
 
-        return null;
+        return Arrays.copyOf(names,index);
 
     }
-
 
 
     public String[] findZipCodersWhoseNamesContainLetter(String letter, String[] zipCoders) {
+        String[] tempArray = new String[zipCoders.length];
 
-        return null;
+        int index = 0;
+        for(int i = 0; i < zipCoders.length; i++){
+            for(int j = 0; j < zipCoders[i].length(); j++){
+                if(zipCoders[i].substring(j, j+1).equalsIgnoreCase(letter)){
+                    tempArray[index] = zipCoders[i];
+                    j = zipCoders[i].length();
+                    index++;
+                }
+            }
+        }
+
+        String[] answer = new String[index];
+
+        for(int i = 0; i < answer.length; i++){
+            answer[i] = tempArray[i];
+        }
+
+        return answer;
     }
-
-
 
 
     public String[] removeDuplicateZipcoders(String[] zipcoders) {
 
-        return null;
+        Arrays.sort(zipcoders);
+
+        String zipCodersWithoutDuplicates = "";
+
+        if(zipcoders.length > 0){
+            zipCodersWithoutDuplicates = zipcoders[0];
+        }
+        else{
+            return new String[]{};
+        }
+
+        for (int i = 1; i < zipcoders.length; i++){
+            if(!zipcoders[i].equalsIgnoreCase(zipcoders[i - 1])){
+                zipCodersWithoutDuplicates += " " + zipcoders[i];
+            }
+        }
+        return zipCodersWithoutDuplicates.split(" ");
 
     }
 }
